907717b8606fa7703431581583bce4c8441aeca8
Paul Vinson Brown <paul.brown@mukavait.fi>
2020-01-22 12:15:44 +0200

    Do coding for 3.9

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 
+const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 morgan.token('content', req => JSON.stringify(req.body));
